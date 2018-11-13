af40853ebc883c4e942d5716ae824b9d6c8804fa
os4 <one.stp.4head@gmail.com>
2018-11-13 13:55:13 +0700

    ht6.2

--- ht6/ht6.2/script.js
+++ ht6/ht6.2/script.js
@@ -3,31 +3,50 @@ window.onload = function () {
 	console.log(cells);
 	
 	function randomInt(min, max) {
-		var rand = min - 0.5 + Math.random() * (max - min + 1)
-		rand = Math.round(rand);
-		return rand;
+		var rnd = min + Math.random() * (max - min + 1)
+		rnd = Math.floor(rnd);
+		return rnd;
 	}
 	
 	function getRndColor() {
 		let letters = '0123456789ABCDF'
 		let rndColor = '#';
 		for (let i = 0; i < 6; i++) {
-			rndColor += letters[randomInt(0, 15)];
+			rndColor += letters[randomInt(0, 14)];
 		}
 		console.log(rndColor);
 		return rndColor;
 	}
 	
 	function getRndElem() {
-		rndElem = cells[randomInt(0, cells.length)];
+		rndElem = cells[randomInt(0, cells.length - 1)];
 		console.log(rndElem);
 		return rndElem;
 	}
 	
 	function highLightRndCell() {
-		let elemToHiLight = getRndElem;
+		let elemToHiLight = getRndElem();
+		console.log(`elem to HL: \n`);
+		console.log(elemToHiLight);
 		elemToHiLight.style.backgroundColor = getRndColor();
 	}
+
+	function unHighLightRndCell() {
+		let elemToUnHighLight = getRndElem();
+		console.log(`elem to unHL: \n`);
+		console.log(elemToUnHighLight);
+		if (elemToUnHighLight.style.backgroundColor === "#ccc") {
+			return
+		} else {
+			elemToUnHighLight.style.backgroundColor = "#ccc";
+		}
+	}
+
+	function itsPartyTime () {
+		let colorTimer = setInterval (highLightRndCell, 2000);
+		let timerID = setTimeout (() => console.log('timer'), 1000);
+		let unColorTimer = setInterval (unHighLightRndCell, 2000);
+	}
 	
-	highLightRndCell();
+	// itsPartyTime();
 }
